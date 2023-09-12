8ac803a10dda096c07be06c47e7adf4ddc789d44
makachikava <chikavamakanaka@gmail.com>
2023-09-12 12:06:27 -0700

    created chat bubbles & styled them

--- music_assistant/components/chatComponent.tsx
+++ music_assistant/components/chatComponent.tsx
@@ -2,7 +2,21 @@
 
 export default function chatComponent() {
     return ( 
-    <div>
+<div>
+
+<div className="flex-grow overflow-y-auto">
+<div className="flex flex-col space-y-2 p-4">
+
+    {/* <!-- Individual chat messages --> */}
+    <div className="self-end bg-blue-500 text-white rounded-lg p-2 flex items-center">
+        <p>This is a sender message</p>
+    </div>
+    <div className="self-start bg-gray-200 rounded-lg p-2 flex items-center">
+        <p>This is a receiver message</p>
+    </div>
+    
+</div>
+</div>
 
         <div className="p-4 flex items-center">
         <input 
@@ -12,7 +26,7 @@ export default function chatComponent() {
             />
         <button className="ml-2 bg-blue-500 text-white text-lg px-4 py-2 rounded-lg hover:bg-blue-300">Send</button>
         </div>
-    </div>
+</div>
 
 );
 }
