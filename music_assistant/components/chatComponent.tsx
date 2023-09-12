c21d7f852fc911eab6608283e2a498984695efda
makachikava <chikavamakanaka@gmail.com>
2023-09-12 12:30:54 -0700

    added form to chatcomponent & integrated handlers from vercel ai sdk

--- music_assistant/components/chatComponent.tsx
+++ music_assistant/components/chatComponent.tsx
@@ -1,6 +1,13 @@
 "use client"
+import { useChat } from "ai/react";
 
 export default function chatComponent() {
+    // Vercel AI SDK (ai package) useChat()
+    // useChat -> handles messages for us, user input, handling user submits, etc.
+    const { input, handleInputChange, handleSubmit, isLoading, messages } = useChat()
+    // messages -> [user asks question, gpt-4 response, user asks again, gpt responds]
+    console.log(messages)
+    console.log(input)
     return ( 
 <div>
 
@@ -14,18 +21,25 @@ export default function chatComponent() {
     <div className="self-start bg-gray-200 rounded-lg p-2 flex items-center">
         <p>This is a receiver message</p>
     </div>
-    
+
 </div>
 </div>
 
+    {/* Input section */}
         <div className="p-4 flex items-center">
-        <input 
-            type="text" 
-            placeholder="Write your message..." 
-            className="border border-gray-300 rounded-lg px-4 py-2 w-full"
-            />
-        <button className="ml-2 bg-blue-500 text-white text-lg px-4 py-2 rounded-lg hover:bg-blue-300">Send</button>
+            <form className="p-4 w-full flex items-center" onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    placeholder="Write your message..." 
+                    className="border border-gray-300 rounded-lg px-4 py-2 w-full"
+                    value={input}
+                    onChange={handleInputChange}
+                    />
+                <button className="ml-2 bg-blue-500 text-white text-lg px-4 py-2 rounded-lg hover:bg-blue-300">Send</button>
+            </form>
         </div>
+
+
 </div>
 
 );
