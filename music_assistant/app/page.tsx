871128894136f52e001f6275270d4d1fa96bdb73
makachikava <chikavamakanaka@gmail.com>
2025-01-30 20:13:58 -0800

    new api key and tab title change

--- music_assistant/app/page.tsx
+++ music_assistant/app/page.tsx
@@ -2,6 +2,9 @@ import ChatComponent from '@/components/chatComponent'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <head>
+        <title>MusicAI Assistant</title>
+      </head>
       <div className='bg-slate-50 p-3 w-[800px] rounded-md text-black shadow-xl'>
       <div className='flex gap-1.5 items-center justify-center'>
         <p className="w-2.5 h-2.5 rounded-full bg-green-500 flex"/>
